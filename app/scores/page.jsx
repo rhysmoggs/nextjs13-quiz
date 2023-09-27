b83b48c28732aa9e331eb1b6470dd25ca23aad65
Rhys Morgan <rhysmoggs@hotmail.com>
2023-09-27 15:18:39 +0100

    add api route to fetch

--- app/scores/page.jsx
+++ app/scores/page.jsx
@@ -5,19 +5,22 @@ import Link from 'next/link'
 
 //test quiz api
 async function getScores() {
-  const res = await fetch(process.env.POCKETBASE_URL, { cache: 'no-store' })
-  const data = await res.json()
+  const res = await fetch(
+    'http://127.0.0.1:8090/api/collections/quiz/records?page=1&perPage=30',
+    { cache: 'no-store' }
+  )
+  return res.json()
 
   //array of all records in quiz collection:
-  const allData = await data.items
+  // const allData = await data.items
 
-  return allData
+  // return allData
 }
 
 export default async function HighScores() {
-  const allScores = await getScores()
+  const data = await getScores()
   // //return each score:
-  const scores = await allScores?.map((scores) => {
+  const scores = await data.items?.map((scores) => {
     // console.log('internal clg scores', scores)
     // console.log('internal clg scores.score', scores.score)
     // console.log('internal clg scores.username', scores.username)
