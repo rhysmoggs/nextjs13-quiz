2e66e67dbbca524189e2b64bdee8537dd1b5a820
Rhys Morgan <rhysmoggs@hotmail.com>
2023-11-03 12:45:41 +0000

    added dns to priorotize ipv4 first

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+// added following 2 lines as an attempt to solve deployment bug:
+const dns = require('dns')
+
+dns.setDefaultResultOrder('ipv4first')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
