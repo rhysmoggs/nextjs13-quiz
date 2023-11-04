364ebfa839d6b45633a787579bcdb2625db20640
Rhys Morgan <rhysmoggs@hotmail.com>
2023-11-04 17:59:48 +0000

    remove ip4 config priority for testing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // added following 2 lines as an attempt to solve deployment bug:
-const dns = require('dns')
+// const dns = require('dns')
 
-dns.setDefaultResultOrder('ipv4first')
+// dns.setDefaultResultOrder('ipv4first')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
