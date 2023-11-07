80319d23ac357d06e1bfe3d9b754374f44d515a5
Rhys Morgan <rhysmoggs@hotmail.com>
2023-11-07 14:45:39 +0000

    breakdown to check errors

--- app/components/Score.jsx
+++ app/components/Score.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import styles from '../page.module.css'
 
-function Score({ singleScore }) {
-  // const { id, username, score } = singleScore || {}
-  const { id, username, score } = singleScore || null
+// function Score({ singleScore }) {
+//   // const { id, username, score } = singleScore || {}
+//   const { id, username, score } = singleScore || null
+//   return (
+//     //temp styling:
+//     <div className={styles.card}>
+//       <p>{id}</p>
+//       <p>Username: {username}</p>
+//       <p>Score: {score}</p>
+//     </div>
+//   )
+// }
+
+function Score() {
   return (
-    //temp styling:
     <div className={styles.card}>
-      <p>{id}</p>
-      <p>Username: {username}</p>
-      <p>Score: {score}</p>
+      <p>Username:</p>
     </div>
   )
 }
