5668c723803102939f23e28bb3df00971248bb9e
Rhys Morgan <rhysmoggs@hotmail.com>
2023-11-07 14:49:45 +0000

    further breakdown to check errors

--- app/scores/page.jsx
+++ app/scores/page.jsx
@@ -17,6 +17,7 @@ import Link from 'next/link'
 
 //   return allData
 // }
+
 async function getScores() {
   const records = await pb.collection('quiz').getFullList({
     sort: '-created',
@@ -26,27 +27,34 @@ async function getScores() {
   return records
 }
 
+// export default async function HighScores() {
+//   const data = await getScores()
+//   // //return each score:
+//   const scores = await data.map((scores) => {
+//     // console.log('internal clg scores', scores)
+//     // console.log('internal clg scores.score', scores.score)
+//     // console.log('internal clg scores.username', scores.username)
+//     return scores
+//   })
+//   return (
+//     <div>
+//       <h1>High Scores</h1>
+//       <div>
+//         {scores?.map((score) => {
+//           // return <Score key={score.id} singleScore={score} />
+//           return <Score key={score.id} singleScore={score || null} />
+//         })}
+//       </div>
+//       <Link href='/' className={styles.btnLink}>
+//         Home
+//       </Link>
+//     </div>
+//   )
+// }
 export default async function HighScores() {
-  const data = await getScores()
-  // //return each score:
-  const scores = await data.map((scores) => {
-    // console.log('internal clg scores', scores)
-    // console.log('internal clg scores.score', scores.score)
-    // console.log('internal clg scores.username', scores.username)
-    return scores
-  })
   return (
     <div>
       <h1>High Scores</h1>
-      <div>
-        {scores?.map((score) => {
-          // return <Score key={score.id} singleScore={score} />
-          return <Score key={score.id} singleScore={score || null} />
-        })}
-      </div>
-      <Link href='/' className={styles.btnLink}>
-        Home
-      </Link>
     </div>
   )
 }
