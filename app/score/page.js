746a0b90e4fe45d33c58fe24428491f0e9a6a4d6
Rhys Morgan <rhysmoggs@hotmail.com>
2023-11-16 14:46:15 +0000

    add correct prefix for dev env variables and added fetch and data displaying in Score page

--- app/score/page.js
+++ app/score/page.js
@@ -1,53 +1,44 @@
 //client side render, no use of hooks/state
 'use client'
-import React, { useState } from 'react'
-import styles from '../page.module.css'
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
-  const [scoreData, getScoreData] = useState(null)
-  //function to fetch scores:
-  const getScores = async () => {
-    const res = await fetch(
-      // `${process.env.POCKETBASE_URL}/api/collections/quiz/records?page=1&perPage=30`,
-      // { cache: 'no-store' }
-      // process.env.POCKETBASE_URL +
-      //   '/api/collections/quiz/records?page=1&perPage=30',
-      // { cache: 'no-store' }
-      'http://127.0.0.1:8090/api/collections/quiz/records?page=1&perPage=30',
-      {
-        cache: 'no-store',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      }
-    )
-    console.log('res: ', res)
-    const data = await res.json()
-    console.log('data: ', data)
+  const [scoreData, setScoreData] = useState(null)
 
-    const allData = await data.items
-    console.log('allData clg: ', allData)
-    getScoreData(allData)
-  }
+  useEffect(() => {
+    //function to fetch scores:
+    const getScores = async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_POCKETBASE_URL}/api/collections/quiz/records?page=1&perPage=30`,
+        // process.env.NEXT_PUBLIC_POCKETBASE_URL +
+        //   '/api/collections/quiz/records?page=1&perPage=30',
+        // { cache: 'no-store' }
+        // 'http://127.0.0.1:8090/api/collections/quiz/records?page=1&perPage=30',
+        {
+          cache: 'no-store',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      console.log('res: ', res)
+      const data = await res.json()
+      console.log('data: ', data)
 
-  console.log('SCORE DATA: ', scoreData)
-
-  //what is the purpose of this function?:
-  const displayScores = async () => {
-    const res = await getScores()
-    //currently showing as undefined:
-    console.log(res)
-  }
+      const allData = await data.items
+      console.log('allData clg: ', allData)
+      setScoreData(allData)
+    }
+    getScores()
+  }, [])
 
   //instead of returning this, just export current code as components and create new Page, then import and <Score /> it.
   return (
     <div>
-      <button onClick={displayScores} className={styles.btn}>
-        Add new High Score
-      </button>
+      <h1>List of High Scores:</h1>
       {/* test just to display data: */}
-      <p>{JSON.stringify(scoreData)}</p>
+      {scoreData ? <p>{JSON.stringify(scoreData)}</p> : <p>Loading...</p>}
     </div>
   )
 }
