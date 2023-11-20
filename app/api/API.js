50e73d6b4ffc07cc0ecb5ee7cf0571e13adafef4
Rhys Morgan <rhysmoggs@hotmail.com>
2023-11-20 13:57:53 +0000

    add devURL and prodURLs to determine fetch api location

--- app/api/API.js
+++ app/api/API.js
@@ -2,15 +2,25 @@
 
 require('dotenv').config()
 
+// function chooseEnv() {
+//   if ((process.env.DEVELOPMENT = 'True')) {
+//     const url = process.env.NEXT_PUBLIC_POCKETBASE_URL
+//     return url
+//   }
+//   console.log('URL IS: ', url)
+//   //need to return url here:
+// }
+
+const devURL = process.env.NEXT_PUBLIC_POCKETBASE_URL
+const prodURL = process.env.POCKETBASE_URL
+
+const URL = process.env.NODE_ENV === 'development' ? devURL : prodURL
+
 const getDataFromAPI = async () => {
   //function to fetch scores:
   const res = await fetch(
     // `${process.env.NEXT_PUBLIC_POCKETBASE_URL}/api/collections/quiz/records?page=1&perPage=30`,
-    `${process.env.POCKETBASE_URL}/api/collections/quiz/records?page=1&perPage=30`,
-    // process.env.NEXT_PUBLIC_POCKETBASE_URL +
-    //   '/api/collections/quiz/records?page=1&perPage=30',
-    // { cache: 'no-store' }
-    // 'http://127.0.0.1:8090/api/collections/quiz/records?page=1&perPage=30',
+    `${URL}/api/collections/quiz/records?page=1&perPage=30`,
     {
       cache: 'no-store',
       headers: {
