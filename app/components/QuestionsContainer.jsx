4d9d43e7823faa7048268b31b387165efe764375
Rhys Morgan <rhysmoggs@hotmail.com>
2023-12-11 14:05:28 +0000

    return mapped questions to test QuestionsContainer and api fetch

--- app/components/QuestionsContainer.jsx
+++ app/components/QuestionsContainer.jsx
@@ -1,211 +1,58 @@
-'use client'
-import Card from '../components/Card'
 import GetQuizQs from '../api/GetQuizQs'
-import { useEffect, useState } from 'react'
-import styles from '../page.module.css'
-import Link from 'next/link'
-import AddScore from '../components/AddScore'
-import getEnvironment from '../getEnvironment.config.js'
 
 const QuestionsContainer = async () => {
-  const [questions, setQuestions] = useState([])
-  const [activeQuestion, setActiveQuestion] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [progress, setProgress] = useState(0)
-  const [score, setScore] = useState(0)
-  const [started, setStarted] = useState(false)
-  const [endGame, setEndGame] = useState(false)
-  const [disabledClass, setDisabledClass] = useState(false)
-  // const [count, setCount] = useState(5)
-
-  //attempt at adding timer:
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setCount((prevCount) => prevCount - 1)
-  //   }, 1000)
-  //   setCount(3)
-  //   return () => clearInterval(intervalId)
-  // }, [questions])
-
   //fetch api data:
-  const getQuizAPI = async () => {
-    try {
-      setLoading(true)
-      const results = await GetQuizQs()
+  const results = await GetQuizQs()
 
-      const questions = results.map((q) => {
-        const question = q.question
+  const questions = results.map((q) => {
+    const question = q.question
 
-        //add
-        const correct = { answer: q.correct_answer, rightAnswer: true }
-        const incorrect = q.incorrect_answers.map((i) => {
-          return i
-        })
+    //add
+    const correct = { answer: q.correct_answer, rightAnswer: true }
+    const incorrect = q.incorrect_answers.map((i) => {
+      return i
+    })
 
-        const lastIncorrect = incorrect.map((inc) => {
-          return { answer: inc, rightAnswer: false }
-        })
-        // console.log(lastCorrect)
-        const answers = [...lastIncorrect, correct]
+    const lastIncorrect = incorrect.map((inc) => {
+      return { answer: inc, rightAnswer: false }
+    })
+    // console.log(lastCorrect)
+    const answers = [...lastIncorrect, correct]
 
-        //randomize order of answers:
-        function shuffle(array) {
-          let currentIndex = array.length,
-            randomIndex
+    //randomize order of answers:
+    function shuffle(array) {
+      let currentIndex = array.length,
+        randomIndex
 
-          // While there remain elements to shuffle.
-          while (currentIndex > 0) {
-            // Pick a remaining element.
-            randomIndex = Math.floor(Math.random() * currentIndex)
-            currentIndex--
+      // While there remain elements to shuffle.
+      while (currentIndex > 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
 
-            // And swap it with the current element.
-            ;[array[currentIndex], array[randomIndex]] = [
-              array[randomIndex],
-              array[currentIndex],
-            ]
-          }
+        // And swap it with the current element.
+        ;[array[currentIndex], array[randomIndex]] = [
+          array[randomIndex],
+          array[currentIndex],
+        ]
+      }
 
-          return array
-        }
-        shuffle(answers)
-
-        return { question, answers }
-      })
-
-      setLoading(false)
-
-      setQuestions(questions)
-      newQuestion(questions)
-      setScore(0)
-      setStarted(true)
-      //can improve this by toggling? if started is true then end is false?
-      setEndGame(false)
-      // console.log(questions)
-    } catch (error) {
-      console.error('Error: ', error)
+      return array
     }
-  }
+    shuffle(answers)
 
-  useEffect(() => {
-    getQuizAPI()
-  }, [])
-
-  // generate new question:
-  const newQuestion = (questions) => {
-    if (progress === totalQuestions) {
-      //reset score, progress and questions to 0/null:
-      setQuestions([])
-      setProgress(0)
-      // console.log('end game')
-      //run end game function:
-
-      //switch Start Game button txt to Restart:
-      setEndGame(true)
-      //hide question quiz area:
-      setStarted(false)
-    } else {
-      //reset answer state:
-
-      //take first question set:
-      const newQ = questions[0]
-      setActiveQuestion(newQ)
-      //remove question from set of questions:
-      const newList = questions.slice(1)
-      // update:
-      setQuestions(newList)
-      setProgress(progress + 1)
-    }
-  }
-
-  // check if selected answer is correct or not:
-  const givenAnswer = (answer) => {
-    setDisabledClass(true)
-    const defoCorrect = activeQuestion['answers'].find(
-      (item) => item.rightAnswer === true
-    )
-
-    if (answer.rightAnswer === defoCorrect.rightAnswer) {
-      // console.log('yeeee buddy!')
-
-      setScore(score + 2500)
-    } else {
-      // console.log('nahhh')
-      //write logic for wrong answer or visual UX?
-    }
-    setTimeout(() => {
-      newQuestion(questions)
-      setDisabledClass(false)
-    }, 1000)
-  }
-
-  const addHighScore = async (data) => {
-    //function to fetch scores:
-    try {
-      const response = await fetch(
-        `${getEnvironment.currentEnvironment}/api/collections/quiz/records`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        }
-      )
-      const result = await response.json()
-      return result
-    } catch (error) {
-      console.error('Error: ', error)
-    }
-  }
-
-  const totalQuestions = 2
+    return { question, answers }
+  })
+  console.log(questions.question)
 
   return (
-    <>
-      {loading && <h2>Loading...</h2>}
-      {started && (
-        <>
-          <p>Time left: {count} seconds</p>
-          {/* if the game has started then show questions */}
-
-          <h2>
-            Question: {progress} / {totalQuestions}
-          </h2>
-          <h2 className={styles.scoreTally}>{score} miles travelled</h2>
-          <h2>{activeQuestion && activeQuestion.question}</h2>
-          <div className={styles.grid}>
-            {activeQuestion &&
-              activeQuestion.answers.map((answer, index) => (
-                <Card
-                  disabled={disabledClass}
-                  answer={answer}
-                  key={index}
-                  givenAnswer={givenAnswer}
-                  progress={progress}
-                />
-              ))}
-          </div>
-        </>
-      )}
-      {endGame && (
-        <>
-          {loading && <p>Loading...</p>}
-
-          <h3>Congratulations!</h3>
-          <h2 className={styles.scoreTally}>{score} miles travelled</h2>
-          <AddScore score={score} addHighScore={addHighScore} />
-
-          <Link href='/game' className={styles.btn}>
-            Start
-          </Link>
-
-          <Link href='/scores' className={styles.btn}>
-            High Scores
-          </Link>
-        </>
-      )}
-    </>
+    <div>
+      {questions.map((question) => console.log(question))}
+      {questions.map((question, id) => {
+        return <ul key={id}>Question: {question.question}</ul>
+      })}
+      {questions.answers}
+    </div>
   )
 }
 
