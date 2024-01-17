b7f5d5006945f53e2498058ff191b28c2e2a7ad4
Rhys Morgan <rhysmoggs@hotmail.com>
2024-01-17 21:07:00 +0000

    add randomize question order logic to GetQuizQs.js

--- app/api/GetQuizQs.js
+++ app/api/GetQuizQs.js
@@ -19,6 +19,28 @@ const GetQuizQs = async () => {
       })
       const answers = [...lastIncorrect, correct]
 
+      //randomize order of answers:
+      function shuffle(array) {
+        let currentIndex = array.length,
+          randomIndex
+
+        // While there remain elements to shuffle.
+        while (currentIndex > 0) {
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex)
+          currentIndex--
+
+          // And swap it with the current element.
+          ;[array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+          ]
+        }
+
+        return array
+      }
+      shuffle(answers)
+
       return { question, answers }
     })
     return questions
