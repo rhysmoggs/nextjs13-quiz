988c53c63438f071990e2f41e919812b0edc2672
Rhys Morgan <rhysmoggs@hotmail.com>
2024-01-24 13:04:31 +0000

    moved error.js

--- app/game/page.js
+++ app/game/page.js
@@ -1,6 +1,7 @@
 import styles from '../page.module.css'
 import Logo from '../components/Logo'
 import QuestionsContainer from '../components/QuestionsContainer'
+import Error from './error'
 
 function Game() {
   return (
@@ -8,6 +9,7 @@ function Game() {
       <Logo />
       <div className={styles.gameArea}>
         <div className={styles.container}>
+          <Error />
           <QuestionsContainer />
         </div>
       </div>
