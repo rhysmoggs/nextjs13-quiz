ebdff0c492d68fbb429a063fbfbecd05a0c2fe25
Rhys Morgan <rhysmoggs@hotmail.com>
2024-01-26 13:39:26 +0000

    add repsonsive design to score board

--- app/components/Score.jsx
+++ app/components/Score.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../page.module.css'
 
 const Score = ({ ...score }) => {
+  //add logic to hightlight top score in Gold colour
   return (
     <div className={styles.scoreCard}>
       <p>Username: {score.username}</p>
