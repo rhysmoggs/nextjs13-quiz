f4ae32345f0d4c4fb16a2c9112f8d16d2925f94d
Rhys Morgan <rhysmoggs@hotmail.com>
2024-01-30 11:35:08 +0000

    install react-icons and add social links to contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,5 +1,13 @@
 import styles from '../page.module.css'
 import Logo from '../components/Logo'
+import {
+  FaXTwitter,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+  FaYoutube,
+  FaCopyright,
+} from 'react-icons/fa6'
 
 function Contact() {
   return (
@@ -8,6 +16,64 @@ function Contact() {
       <div className={styles.gameArea}>
         <div className={styles.container}>
           <h1>Contact Form & Info:</h1>
+          <div>
+            <ul className={styles.contactList}>
+              <li>
+                <a
+                  href='https://twitter.com/'
+                  target='_blank'
+                  rel='noopener'
+                  aria-label='link to quiz twitter website (opens in new tab)'
+                >
+                  <FaXTwitter />
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.youtube.com/'
+                  target='_blank'
+                  rel='noopener'
+                  aria-label='link to quiz youtube website (opens in new tab)'
+                >
+                  <FaYoutube />
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://www.instagram.com/'
+                  target='_blank'
+                  rel='noopener'
+                  aria-label='link to quiz instagram website (opens in new tab)'
+                >
+                  <FaInstagram />
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://github.com/rhysmoggs'
+                  target='_blank'
+                  rel='noopener'
+                  aria-label='link to personal gitHub profile (opens in new tab)'
+                >
+                  <FaGithub />
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href='https://www.linkedin.com/in/rhysmoggs/'
+                  target='_blank'
+                  rel='noopener'
+                  aria-label='link to personal linkedin profile (opens in new tab)'
+                >
+                  <FaLinkedin />
+                </a>
+              </li>
+            </ul>
+            <p>
+              copyright <FaCopyright /> rhysmoggs 2024
+            </p>
+          </div>
         </div>
       </div>
     </main>
