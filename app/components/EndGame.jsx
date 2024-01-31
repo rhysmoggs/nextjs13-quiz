78e314d3b093b3d4f3a984d931ab6a67e982d00d
Rhys Morgan <rhysmoggs@hotmail.com>
2024-01-31 17:58:08 +0000

    Add EndGame componenet

---
+++ app/components/EndGame.jsx
@@ -0,0 +1,58 @@
+'use client'
+import { useState } from 'react'
+import styles from '../page.module.css'
+import AddScore from './AddScore'
+import Link from 'next/link'
+import getEnvironment from '../getEnvironment.config.js'
+
+const EndGame = () => {
+  const [loading, setLoading] = useState(true)
+  // const [score, setScore] = useState(0)
+  // const [spinner, setSpinner] = useState(true)
+
+  const addHighScore = async (data) => {
+    //function to fetch scores:
+    try {
+      const response = await fetch(
+        `${getEnvironment.currentEnvironment}/api/collections/quiz/records`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        }
+      )
+      const result = await response.json()
+      return result
+    } catch (error) {
+      console.error('Error: ', error)
+    }
+  }
+
+  return (
+    <div className={styles.gameArea}>
+      <div className={styles.container}>
+        {loading && <h2>Loading...</h2>}
+
+        <>
+          <h3>
+            {count <= 0 ? `Unlucky! You ran out of time.` : `Congratulations!`}
+          </h3>
+          <h2 className={styles.scoreTally}>{score} miles travelled</h2>
+          <AddScore score={score} addHighScore={addHighScore} />
+
+          <Link href='/' className={styles.btn}>
+            Home
+          </Link>
+
+          <Link href='/scores' className={styles.btn}>
+            High Scores
+          </Link>
+        </>
+      </div>
+    </div>
+  )
+}
+
+export default EndGame
